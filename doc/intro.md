49019a5c8cc3f51349bc821ec4ce9d95f618a4d4
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-12 19:45:44 +0800

    added project files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to inlet
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/inlet/core_test.clj
@@ -0,0 +1,7 @@
+(ns inlet.core-test
+  (:require [clojure.test :refer :all]
+            [inlet.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
