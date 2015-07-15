d5caf7d45ac15418875cac67a6bbb8736ae610a7
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-15 22:33:34 +0800

    new buffer setup

--- src/inlet/core.clj
+++ src/inlet/core.clj
@@ -15,25 +15,34 @@
 (def DERIVE org.rrd4j.DsType/DERIVE)
 (def ABSOLUTE org.rrd4j.DsType/ABSOLUTE)
 
-(comment
-  (def rrd (RrdDef. "/tmp/test.rrd" 300))
-  (doto rrd
+(def step 1)
 
-    ;; name, type, heartbeat, min, max
-    (.addDatasource "probe-1-temp" GAUGE 600 55 95)
+(def rrd (RrdDef. "/tmp/test.rrd" step))
+(doto rrd
 
-    ;; Consolidation Func, XFilesFactor Steps Rows
-    (.addArchive AVERAGE 0.5 1 600)
-    (.addArchive AVERAGE 0.5 6 700)
-    (.addArchive MAX 0.5 1 600))
+  ;; name, type, heartbeat, min, max
+  (.addDatasource "probe-1-temp" GAUGE 600 Double/NaN Double/NaN)
+  (.addDatasource "probe-2-temp" GAUGE 600 Double/NaN Double/NaN)
 
-  (def rdb (RrdDb. rrd))
+  ;; Consolidation Func, XFilesFactor Steps Rows
+  (.addArchive AVERAGE 0.5 1 (/ (* 24 60 60) step))   ;; one days worth @ 1 sec
+  (.addArchive AVERAGE 0.5 60 (/ (* 24 60 7) step))   ;; 7 days @ 1 min
+  (.addArchive AVERAGE 0.5 (* 60 60) (/ (* 24 7 52) step)) ;; one year @ 1 hour
+  (.addArchive AVERAGE 0.5 (* 60 60 24) (/ (* 7 52 20) step)) ;; 20 years @ 1 day
+  (.addArchive MAX 0.5 1 600))
 
-  (def sample (.createSample rdb))
-  (.setTime sample 0)
-  (.setValue sample "probe-1-temp" (double 60))
+(def rdb (RrdDb. rrd))
 
-)
+(def sample (.createSample rdb))
+(.setTime sample 0)
+(.setValue sample "probe-1-temp" (double 60))
+
+;; writes the file
+(.close rdb)
+
+
+
+
 
 
 (defn data [req]
