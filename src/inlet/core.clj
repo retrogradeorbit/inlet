bd488a2ea1fec2068e9f802b38c6cb2d83a2fbc4
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-16 22:23:13 +0800

    generate graph file

--- src/inlet/core.clj
+++ src/inlet/core.clj
@@ -3,7 +3,8 @@
             [clojure.pprint :refer [pprint]]
             [org.httpkit.server :refer [run-server]]
             [ring.middleware.params :refer [wrap-params]]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clojure.java.io :as io])
   (:import [org.rrd4j.core RrdDef RrdDb Sample]
            [org.rrd4j.graph  RrdGraph RrdGraphDef]
            [org.rrd4j.ConsolFun]
@@ -59,27 +60,92 @@
     (.datasource "bytes" "/tmp/test.rrd" "probe-1-temp" AVERAGE)
     (.hrule 2568 Color/GREEN "hrule")
     (.setImageFormat "png"))
-  (def graph  (RrdGraph. rrdgdef)))
+  (def graph  (RrdGraph. rrdgdef))
+
+)
 
 
 
+(defn filename [host]
+  (str "/tmp/rrd/" host ".rrd"))
 
+(defn make-new-rrd [file earliest first-data]
+  (let [d (RrdDef. (str file) earliest 1)
+        step 1]
+    (doall
+     (for [n (keys first-data)]
+       (do
+         (println "name:" n)
+         (.addDatasource d n COUNTER 600 Double/NaN Double/NaN))))
+    (doto d
+      (.addArchive AVERAGE 0.5 1 (/ (* 24 60 60) step)) ;; one day @ 1 sec
+      (.addArchive AVERAGE 0.5 60 (/ (* 24 60 7) step)) ;; 7 days @ 1 min
+      (.addArchive AVERAGE 0.5 (* 60 60) (/ (* 24 7 52) step)) ;; one year @ 1 hour
+      (.addArchive AVERAGE 0.5 (* 60 60 24) (/ (* 7 52 20) step)) ;; 20 years @ 1 day
+      (.addArchive MAX 0.5 1 600))
+    (RrdDb. d)))
+
+(defn make-graph [file earliest latest]
+  (let [rrdgdef (RrdGraphDef.)]
+    (doto rrdgdef
+      (.setWidth 800)
+      (.setHeight 300)
+      (.setFilename "/tmp/test.png")
+      (.setStartTime earliest)
+      (.setEndTime latest)
+      (.setTitle "My Title")
+      (.setVerticalLabel "bytes")
+      (.setAltAutoscale false)
+      ;(.setAltAutoscaleMin true)
+      ;(.setAltAutoscaleMax true)
+      (.setMinValue 1)
+      (.setMaxValue 0)
+
+      (.datasource "bytes" file "INPUT" AVERAGE)
+      (.area "bytes" Color/BLUE)
+      (.hrule 0.6  Color/GREEN "hrule")
+      (.setImageFormat "png"))
+    (RrdGraph. rrdgdef)))
+
+(defn proc-data [data]
+  (into {}
+        (for [[k v] data]
+          [(/ (read-string k) 1000) v])
+        ))
 
 (defn data [{:keys [params] :as req}]
   (let [host (params "host")
         data (-> "data"
                  params
-                 json/read-str)
-        timestamps (sort (map read-string (keys data)))
+                 json/read-str
+                 proc-data)
+        timestamps (sort (keys data))
         earliest (first timestamps)
+        first-data (data earliest)
         nearliest (second timestamps)
         latest (last timestamps)
         step (- nearliest earliest)
+        file (io/file (filename host))
         ]
-    (println host ":" earliest "->" latest "." step)
-)
-  "OK"
-  )
+
+    (let [rrd (if (.exists file)
+                (RrdDb. (str file))
+                (make-new-rrd file earliest first-data))]
+      (doall
+       (for [t timestamps]
+         (let [sample (.createSample rrd)]
+           (.setTime sample  t)
+           ;(.setValue sample "INPUT" (double ((data t) "INPUT")) )
+           ;(.setValue sample "OUTPUT" (double ((data t) "OUTPUT")) )
+           (.setValue sample "INPUT" (double (rand)) )
+           (.setValue sample "OUTPUT" (double (rand)) )
+           ;(println "done" t "," (double ((data t) "INPUT")))
+           ))
+       )
+      (.close rrd)
+      (println rrd "," host ":" earliest "->" latest "." step)
+      (make-graph (str file) earliest latest)
+      "OK")))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
