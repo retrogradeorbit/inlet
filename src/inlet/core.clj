17643c9931e3bc36b246f572d81c70ab3cb7d915
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-18 21:18:50 +0800

    delete obsolete code

--- src/inlet/core.clj
+++ src/inlet/core.clj
@@ -21,51 +21,6 @@
 
 (def step 1)
 
-(comment
-
-  (def rrd (RrdDef. "/tmp/test.rrd" step))
-  (doto rrd
-
-    ;; name, type, heartbeat, min, max
-    (.addDatasource "probe-1-temp" GAUGE 600 Double/NaN Double/NaN)
-    (.addDatasource "probe-2-temp" GAUGE 600 Double/NaN Double/NaN)
-
-    ;; Consolidation Func, XFilesFactor Steps Rows
-    (.addArchive AVERAGE 0.5 1 (/ (* 24 60 60) step))   ;; one days worth @ 1 sec
-    (.addArchive AVERAGE 0.5 60 (/ (* 24 60 7) step))   ;; 7 days @ 1 min
-    (.addArchive AVERAGE 0.5 (* 60 60) (/ (* 24 7 52) step)) ;; one year @ 1 hour
-    (.addArchive AVERAGE 0.5 (* 60 60 24) (/ (* 7 52 20) step)) ;; 20 years @ 1 day
-    (.addArchive MAX 0.5 1 600))
-
-  (def rdb (RrdDb. rrd))
-
-  (def sample (.createSample rdb))
-  (.setTime sample 0)
-  (.setValue sample "probe-1-temp" (double 60))
-
-  ;; writes the file
-  (.close rdb)
-
-
-  ;; make a graph
-  (def rrdgdef (RrdGraphDef.))
-  (doto rrdgdef
-    (.setWidth 800)
-    (.setHeight 150)
-    (.setFilename "/tmp/test.png")
-    (.setStartTime 0)
-    (.setEndTime 1437105502)
-    (.setTitle "My Title")
-    (.setVerticalLabel "bytes")
-    (.datasource "bytes" "/tmp/test.rrd" "probe-1-temp" AVERAGE)
-    (.hrule 2568 Color/GREEN "hrule")
-    (.setImageFormat "png"))
-  (def graph  (RrdGraph. rrdgdef))
-
-)
-
-
-
 (defn filename [host]
   (str "/tmp/rrd/" host ".rrd"))
 
