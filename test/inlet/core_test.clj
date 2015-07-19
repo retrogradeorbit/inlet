91e4e6a405774257c2d9cfb14014c5345a68e1c5
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-19 11:55:04 +0800

    get rid of default test

--- test/inlet/core_test.clj
+++ test/inlet/core_test.clj
@@ -2,9 +2,10 @@
   (:require [clojure.test :refer :all]
             [inlet.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; (deftest a-test
+;;   (testing "FIXME, I fail."
+;;     (is (= 0 1))))
+
 (deftest test-get-label-set
   (testing "get-label-set"
     (is
