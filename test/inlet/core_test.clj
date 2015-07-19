19cf43d99a67241f7f0cce77a5d8ad466d8ecc26
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-19 14:15:17 +0800

    delete proper

--- test/inlet/core_test.clj
+++ test/inlet/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [inlet.core :refer :all]))
 
-;; (deftest a-test
-;;   (testing "FIXME, I fail."
-;;     (is (= 0 1))))
-
 (deftest test-get-label-set
   (testing "get-label-set"
     (is
