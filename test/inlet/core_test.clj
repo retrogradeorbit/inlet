c11b726c0c748b1a8a61ec2e30c2a8b92138cd01
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-19 11:54:15 +0800

    get-label-set tests

--- test/inlet/core_test.clj
+++ test/inlet/core_test.clj
@@ -5,3 +5,17 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+(deftest test-get-label-set
+  (testing "get-label-set"
+    (is
+     (=
+      (get-label-set {123 {:iptables {:input 10}}
+                      234 {:iptables {:input 10}}})
+      #{:iptables}))
+    (is
+     (=
+      (get-label-set {123 {:iptables {:input 10}}
+                      234 {:iptables {:input 10}
+                           :meminfo {:memory 100}}})
+      #{:iptables :meminfo}))))
+
