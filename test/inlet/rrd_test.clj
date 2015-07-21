6a1e75e0744545141f9d19b0550959e5ff7d8067
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-21 20:08:44 +0800

    test for write-data

--- test/inlet/rrd_test.clj
+++ test/inlet/rrd_test.clj
@@ -11,8 +11,9 @@
 
 (deftest test-make-rrd
   (testing "make-rrd"
-    (is (let [rrd
-              (make-rrd "/tmp/test.rrd" 140000 1
+    (is (let [rrd-file "/tmp/test.rrd"
+              rrd
+              (make-rrd rrd-file 140000 1
                         {:INPUT [COUNTER 600 0 200000000]
                          "OUTPUT" [COUNTER 600 0 200000000]}
                         [[AVERAGE 0.5 1 86400]
@@ -20,4 +21,22 @@
                          [AVERAGE 0.5 3600 8736]
                          [AVERAGE 0.5 86400 7280]
                          [MAX 0.5 1 600]])]
-          (.exists (io/file "/tmp/test.rrd"))))))
+          (and (.exists (io/file rrd-file))
+               (> (.length (io/file rrd-file)) 1000000))))))
+
+(deftest test-write-data
+  (testing "write-data"
+    (let [rrd-file "/tmp/test.rrd"
+          rrd
+          (make-rrd rrd-file 140000 1
+                    {:INPUT [COUNTER 600 0 200000000]
+                     :OUTPUT [COUNTER 600 0 200000000]}
+                    [[AVERAGE 0.5 1 86400]
+                     [AVERAGE 0.5 60 10080]
+                     [AVERAGE 0.5 3600 8736]
+                     [AVERAGE 0.5 86400 7280]
+                     [MAX 0.5 1 600]])]
+      (write-data rrd :iptables {140001 {:INPUT 100 :OUTPUT 200}
+                                 140002 {:INPUT 101 :OUTPUT 202}})
+      (is)
+      )))
