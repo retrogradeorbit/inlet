b1848ffb33bcec2cd23ee38410ec75cff0b92746
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-22 20:26:56 +0800

    cleanup

--- src/inlet/rrd.clj
+++ src/inlet/rrd.clj
@@ -117,31 +117,12 @@
     (RrdGraph. rrdgdef)))
 
 
-    ;; (.datasource "input" file "INPUT" AVERAGE)
-    ;;   ;(.area "input" (Color. 0xd0 0x60 0x60 ) "Firewall Input Chain")
-
-    ;; (.area "input" (Color. 0x90 0x90 0xe0 ) "Firewall Input Chain")
-    ;; (.datasource "output" file "OUTPUT" AVERAGE)
-    ;;   ;(.area "output" (Color. 0x70 0x00 0x00) "Firewall Output Chain")
-
-    ;; (.area "output" (Color. 0x00 0x00 0x70) "Firewall Output Chain")
-    ;;   ;(.hrule 0.6  Color/GREEN "hrule")
-
-
-
-
-
-
 (def layout
   {:iptabels
    {:INPUT [COUNTER 600 0 2]
     :OUTPUT [COUNTER 600 0 2]}
    :meminfo
    {:data [GAUGE 600 0 2]}})
-
-#_ (defn make-rrd-label [file label]
-  (make-new-rrd file )
-  )
 
 (defn make-new-rrd [file earliest labels step]
   (make-rrd (str file) earliest step
@@ -153,76 +134,6 @@
              [AVERAGE 0.5 86400 7280]
              [MAX 0.5 1 600]]))
 
-#_ (defn make-new-rrd [file earliest labels step]
-  (let [d (RrdDef. (str file) earliest step)]
-    (println "!!!" (.dump d))
-    (doall
-     (for [n labels]
-       (do
-         (println "name:" n)
-         (.addDatasource d n COUNTER 600 0 200000000))))
-    (doto d
-      (.addArchive AVERAGE 0.5 1 (/ (* 24 60 60) step)) ;; one day @ 1 sec
-      (.addArchive AVERAGE 0.5 60 (/ (* 24 60 7) step)) ;; 7 days @ 1 min
-      (.addArchive AVERAGE 0.5 (* 60 60) (/ (* 24 7 52) step)) ;; one year @ 1 hour
-      (.addArchive AVERAGE 0.5 (* 60 60 24) (/ (* 7 52 20) step)) ;; 20 years @ 1 day
-      (.addArchive MAX 0.5 1 600))
-    (RrdDb. d)))
-
-;; (defn make-graph [file imagefile earliest latest]
-;;   (let [rrdgdef (RrdGraphDef.)]
-;;     (doto rrdgdef
-;;       (.setWidth 900)
-;;       (.setHeight 200)
-;;       (.setFilename imagefile)
-;;       (.setStartTime earliest)
-;;       (.setEndTime latest)
-;;       (.setTitle "Firewall Traffic")
-;;       (.setVerticalLabel "bytes")
-
-;;       ;; hide bevel
-;;       (.setColor RrdGraphConstants/COLOR_SHADEA Color/WHITE)
-;;       (.setColor RrdGraphConstants/COLOR_SHADEB Color/WHITE)
-
-;;       ;; background frame
-;;       (.setColor RrdGraphConstants/COLOR_BACK Color/WHITE)
-
-;;       ;; graph background
-;;       (.setColor RrdGraphConstants/COLOR_CANVAS (Color. 0xf8 0xf8 0xff))
-
-;;       ;; major grid
-;;       (.setColor RrdGraphConstants/COLOR_MGRID (Color. 0x50 0x00 0x00))
-
-;;       ;; frame border and minor grid
-;;       (.setColor RrdGraphConstants/COLOR_GRID (Color. 0xa0 0xa0 0xa0))
-
-;;       ;; minor grid
-;;       (.setNoMinorGrid true)
-
-;;       (.setAntiAliasing true)
-
-;;       (.setShowSignature false)
-
-;;       (.setNoLegend false)
-
-
-
-;;       ;; (.setAltAutoscale true)
-;;       ;; (.setAltAutoscaleMin false)
-;;       ;; (.setAltAutoscaleMax false)
-
-;;       ;(.setMinValue 0)
-;;       ;(.setMaxValue 1000000)
-
-;;       (.datasource "input" file "INPUT" AVERAGE)
-;;       ;(.area "input" (Color. 0xd0 0x60 0x60 ) "Firewall Input Chain")
-;;       (.area "input" (Color. 0x90 0x90 0xe0 ) "Firewall Input Chain")
-;;       (.datasource "output" file "OUTPUT" AVERAGE)
-;;       ;(.area "output" (Color. 0x70 0x00 0x00) "Firewall Output Chain")
-;;       (.area "output" (Color. 0x00 0x00 0x70) "Firewall Output Chain")
-;;       ;(.hrule 0.6  Color/GREEN "hrule")
-;;       (.setImageFormat "png"))
-;;     (RrdGraph. rrdgdef)))
 
 (defn new-and-open [filename labels step earliest]
   (if (.exists filename)
