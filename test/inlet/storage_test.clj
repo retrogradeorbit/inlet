e84c887068439d1255394a4cf4910aad42f74430
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-26 20:29:07 +0800

    test for storage

---
+++ test/inlet/storage_test.clj
@@ -0,0 +1,13 @@
+(ns inlet.storage-test
+  (:require [clojure.test :refer :all]
+            [inlet.storage :refer :all]
+            [clj-time.core :as time]))
+
+(deftest test-make-filename
+  (testing "make-filename creates a correctly formatted filename"
+    (binding [croak.storage/*image-storage-path* "/storage/"
+              croak.storage/*time-format* "yyyy-MM-dd-HH:mm:ss.SSS"]
+      (is
+       (=
+        (str (make-filename (time/date-time 1975 11 20 6 30 30 0)))
+        "/storage/data-1975-11-20-06:30:30.000.edn")))))
